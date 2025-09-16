71f3ddf51a2ed4b3c3f0655e70ca933269f21012
alozowick <alozowick001@student.butte.edu>
2025-09-16 20:24:34 +0000

    1

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,1 +1,12 @@
+#include <cstdio>
 
+int main(){
+    int x=1;
+    bool b=true;
+    //boolean value
+    std::printf("%i",b);
+    //booleans are numbers
+    std::printf("%i",x);
+    std::printf("hello world");
+    return 0;
+}
